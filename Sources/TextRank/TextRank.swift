26d392fced8d70d9f557db44eec76bfd195dda6d
Josh Cook <joshuacook0023@gmail.com>
2021-01-03 09:24:04 -0800

    test: TextRank similarity function

--- Sources/TextRank/TextRank.swift
+++ Sources/TextRank/TextRank.swift
@@ -38,8 +38,8 @@ class TextRank {
 extension TextRank {
     func similarity(_ a: inout Sentence, _ b: inout Sentence) -> Float {
         if a.words.count == 0 || b.words.count == 0 { return 0.0 }
-        let commonWordCount = Float(a.words.union(b.words).count)
-        let totalWordCount = log10(Float(a.words.count)) + log10(Float(a.words.count))
+        let commonWordCount = Float(a.words.intersection(b.words).count)
+        let totalWordCount = log10(Float(a.words.count)) + log10(Float(b.words.count))
         return totalWordCount == 0.0 ? 0.0 : commonWordCount / totalWordCount
     }
 }
--- Tests/TextRankTests/TextRankTests.swift
+++ Tests/TextRankTests/TextRankTests.swift
@@ -6,4 +6,38 @@ class TextRankTests: XCTestCase {
         let textRank = TextRank(text: "Here is some text. There are two sentences.")
         XCTAssertEqual(textRank.sentences.count, 2)
     }
+
+    func testComparingSentences() {
+        let textRank = TextRank(text: "")
+
+        var s1 = Sentence(text: "dog bear sheep lion")
+        var s2 = Sentence(text: "dog bear sheep lion")
+        var sim = textRank.similarity(&s1, &s2)
+        XCTAssertEqual(sim, 4.0 / (log10(4.0) + log10(4.0)))
+
+        s1 = Sentence(text: "dog bear sheep lion")
+        s2 = Sentence(text: "dog bear sheep")
+        sim = textRank.similarity(&s1, &s2)
+        XCTAssertEqual(sim, 3.0 / (log10(4.0) + log10(3.0)))
+
+        s1 = Sentence(text: "dog bear sheep lion to there")
+        s2 = Sentence(text: "dog bear sheep we them")
+        sim = textRank.similarity(&s1, &s2)
+        XCTAssertEqual(sim, 3.0 / (log10(4.0) + log10(3.0)))
+
+        s1 = Sentence(text: "")
+        s2 = Sentence(text: "dog bear sheep we them")
+        sim = textRank.similarity(&s1, &s2)
+        XCTAssertEqual(sim, 0.0)
+
+        s1 = Sentence(text: "there will")
+        s2 = Sentence(text: "dog bear sheep we them")
+        sim = textRank.similarity(&s1, &s2)
+        XCTAssertEqual(sim, 0.0)
+
+        s1 = Sentence(text: "fox peacock there will")
+        s2 = Sentence(text: "dog bear sheep we them")
+        sim = textRank.similarity(&s1, &s2)
+        XCTAssertEqual(sim, 0.0)
+    }
 }
